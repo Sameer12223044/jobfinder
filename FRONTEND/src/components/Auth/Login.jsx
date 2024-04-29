d6f5a6ac44c64c1a6977990a6b74913164506fa4
Sameer Sharif Chauhan <119564165+Sameer12223044@users.noreply.github.com>
2024-04-29 21:02:54 +0530

    Update Login.jsx

--- FRONTEND/src/components/Auth/Login.jsx
+++ FRONTEND/src/components/Auth/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://jobfinder-9slc.onrender.com/api/v1/user/login",
         {  email, role, password },
         {
           headers: {
